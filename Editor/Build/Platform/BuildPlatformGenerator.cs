dffa34f3222aa816eac594c76b1c2097de590a98
Chase Pettit <chasepettit@gmail.com>
2016-04-15 11:38:40 -0400

    Moved some files.

---
+++ Editor/Build/Platform/BuildPlatformGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace UnityBuild
+{
+
+public static class BuildPlatformGenerator
+{
+    [MenuItem("Build/Generate/BuildPlatform", false, 100)]
+    private static void GenerateBuildSettings()
+    {
+        Generator.Generate("BuildCustomPlatform.cs", "BuildPlatform", "BuildPlatformTemplate");
+    }
+}
+
+}
--- Editor/Build/Settings/BuildSettingsGenerator.cs
+++ Editor/Build/Settings/BuildSettingsGenerator.cs
@@ -1,8 +1,4 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-using System.Reflection;
-using System.IO;
+﻿using UnityEditor;
 
 namespace UnityBuild
 {
@@ -12,34 +8,7 @@ public static class BuildSettingsGenerator
     [MenuItem("Build/Generate/BuildSettings", false, 100)]
     private static void GenerateBuildSettings()
     {
-        string directory = EditorUtility.OpenFolderPanel("Choose location for ProjectBuildSettings.cs (MUST BE AN EDITOR DIRECTORY)", Application.dataPath, "");
-
-        // If user cancelled, we can't do anything.
-        if (string.IsNullOrEmpty(directory))
-        {
-            return;
-        }
-        else if (!directory.Contains(Path.AltDirectorySeparatorChar + "Editor") && !directory.Contains(Path.DirectorySeparatorChar + "Editor" + Path.DirectorySeparatorChar))
-        {
-            Debug.LogError("BuildSettings file must be in an Editor directory/sub-directory");
-            return;
-        }
-
-        string filePath = Path.Combine(directory, "ProjectBuildSettings.cs");
-
-        string templateFilePath = string.Empty;
-        foreach (var file in Directory.GetFiles(Application.dataPath, "*.txt", SearchOption.AllDirectories))
-        {
-            if (Path.GetFileNameWithoutExtension(file) == "BuildSettingsTemplate")
-            {
-                templateFilePath = file;
-                break;
-            }
-        }
-
-        File.WriteAllText(filePath, File.ReadAllText(templateFilePath));
-
-        AssetDatabase.Refresh();
+        Generator.Generate("ProjectBuildSettings.cs", "BuildSettings", "BuildSettingsTemplate");
     }
 }
 
