639a9cc5fff2f7b517fb73fb11849f671d79ef06
Chase Pettit <chasepettit@gmail.com>
2016-04-15 08:46:29 -0400

    Start build action and CLI classes.

---
+++ Editor/Build/BuildAction.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public abstract class BuildAction
+{
+    public abstract void Execute();
+}
---
+++ Editor/Build/BuildCLI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UnityBuild
+{
+
+public static class BuildCLI
+{
+    private const string OutputPathParameter = "-out";
+    private const string BuildVersionParameter = "-buildVersion";
+    private const string BuildTargetParameter = "-target";
+
+}
+
+}
