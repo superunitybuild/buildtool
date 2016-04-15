675a405cdbcc7856632b7a72ae91ccd97a6154f2
Chase Pettit <chasepettit@gmail.com>
2016-04-15 08:44:06 -0400

    Add virtual pre/post build methods.

--- Editor/Settings/BuildSettings.cs
+++ Editor/Settings/BuildSettings.cs
@@ -11,28 +11,13 @@ public abstract class BuildSettings
     public abstract string[] scenesInBuild { get; }
     public abstract string[] copyToBuild { get; }
 
-    //// The name of executable file (e.g. mygame.exe, mygame.app)
-    //public const string BIN_NAME = "mygame";
+    public virtual void PreBuild()
+    {
+    }
 
-    //// The base path where builds are output.
-    //// Path is relative to the Unity project's base folder unless an absolute path is given.
-    //public const string BIN_PATH = "bin";
-
-    //// A list of scenes to include in the build. The first listed scene will be loaded first.
-    //public static string[] scenesInBuild = new string[] {
-    //    // "Assets/Scenes/scene1.unity",
-    //    // "Assets/Scenes/scene2.unity",
-    //    // ...
-    //};
-
-    //// A list of files/directories to include with the build. 
-    //// Paths are relative to Unity project's base folder unless an absolute path is given.
-    //public static string[] copyToBuild = new string[] {
-    //    // "DirectoryToInclude/",
-    //    // "FileToInclude.txt",
-    //    // ...
-    //};
-
-
+    public virtual void PostBuild()
+    {
+    }
 }
+
 }
