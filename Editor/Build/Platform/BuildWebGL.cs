8dfd95027ac5e96c03c782879e673b5e488bf037
Chase Pettit <chasepettit@gmail.com>
2018-04-24 20:12:28 -0400

    WebGL Build Platform.

---
+++ Editor/Build/Platform/BuildWebGL.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+
+namespace SuperSystems.UnityBuild
+{
+
+[System.Serializable]
+public class BuildWebGL : BuildPlatform
+{
+    #region Constants
+
+    private const string _name = "WebGL";
+    private const string _binaryNameFormat = "{0}";
+    private const string _dataDirNameFormat = "{0}_Data";
+    private const BuildTargetGroup _targetGroup = BuildTargetGroup.WebGL;
+
+    #endregion
+
+    public BuildWebGL()
+    {
+        enabled = false;
+        Init();
+    }
+
+    public override void Init()
+    {
+        platformName = _name;
+        binaryNameFormat = _binaryNameFormat;
+        dataDirNameFormat = _dataDirNameFormat;
+        targetGroup = _targetGroup;
+
+        if (architectures == null || architectures.Length == 0)
+        {
+            architectures = new BuildArchitecture[] { 
+                new BuildArchitecture(BuildTarget.WebGL, "WebGL", true),
+            };
+        }
+    }
+}
+
+}
