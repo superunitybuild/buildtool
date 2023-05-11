b89f560332577004af4454a155f4e16adadd7c14
Robin North <robin@robinnorth.co.uk>
2023-05-11 13:34:14 +0100

    Update default build output path to include `$SCRIPTING_BACKEND`

--- Editor/Build/Settings/BasicSettings.cs
+++ Editor/Build/Settings/BasicSettings.cs
@@ -7,7 +7,7 @@ namespace SuperUnityBuild.BuildTool
     {
         [FilePath(true, true, "Choose location for build output")]
         public string baseBuildFolder = "Builds";
-        [Tooltip("Recognized tokens for the build path: $YEAR, $MONTH, $DAY, $TIME, $RELEASE_TYPE, $PLATFORM, $ARCHITECTURE, $VARIANTS, $DISTRIBUTION, $VERSION, $BUILD, $PRODUCT_NAME, $BACKEND")] public string buildPath = "$VERSION/$RELEASE_TYPE/$PLATFORM/$ARCHITECTURE";
+        [Tooltip("Recognized tokens for the build path: $YEAR, $MONTH, $DAY, $TIME, $RELEASE_TYPE, $PLATFORM, $ARCHITECTURE, $VARIANTS, $DISTRIBUTION, $VERSION, $BUILD, $PRODUCT_NAME, $SCRIPTING_BACKEND")] public string buildPath = "$VERSION/$RELEASE_TYPE/$PLATFORM/$ARCHITECTURE/$SCRIPTING_BACKEND";
         public bool openFolderPostBuild = true;
     }
 }
