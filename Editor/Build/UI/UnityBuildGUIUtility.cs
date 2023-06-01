36f2a23da50fcba655ba3862049f6f431e38e23f
Matt <m22mail@comcast.net>
2023-06-01 17:52:26 -0400

    Color adjustments

--- Editor/Build/UI/UnityBuildGUIUtility.cs
+++ Editor/Build/UI/UnityBuildGUIUtility.cs
@@ -32,12 +32,14 @@ namespace SuperUnityBuild.BuildTool
         private GUIStyle _popupStyle;
         private GUIStyle _mainTitleStyle;
         private GUIStyle _dragDropArea;
+        private GUIStyle _buildButtonStyle;
         private GUIStyle _sceneNameLabelStyle;
         private GUIStyle _sceneInfoLabelStyle;
         private GUIStyle _sceneActiveToggleStyle;
 
         private Color32 _defaultBackgroundColor = GUI.backgroundColor;
         private Color32 _mainHeaderColor = new Color32(180, 180, 255, 255);
+        private Color32 _buildButtonColor = new Color32(102, 230, 102, 255);
 
         private GUIContent helpButtonContent;
 
@@ -79,6 +81,10 @@ namespace SuperUnityBuild.BuildTool
             _dragDropArea.stretchWidth = true;
             _dragDropArea.alignment = TextAnchor.MiddleCenter;
             _dragDropArea.normal.textColor = GUI.skin.textField.normal.textColor;
+            
+            _buildButtonStyle = new GUIStyle(GUI.skin.button);
+            _buildButtonStyle.normal.textColor = Color.black;
+            _buildButtonStyle.hover.textColor = new Color32(0, 0, 0, 200);
 
             _sceneNameLabelStyle = new GUIStyle(EditorStyles.largeLabel);
             _sceneNameLabelStyle.margin = new RectOffset(0, 0, 0, 0);
@@ -131,6 +137,25 @@ namespace SuperUnityBuild.BuildTool
             {
                 OpenHelp(anchor);
             }
+        }
+
+        public static bool BuildButton(string text, int minHeight = 0)
+        {
+            GUI.backgroundColor = instance._buildButtonColor;
+
+            bool buttonPress;
+            if(minHeight > 0)
+            {
+                buttonPress = GUILayout.Button(text, instance._buildButtonStyle, GUILayout.ExpandWidth(true), GUILayout.MinHeight(minHeight));
+            }
+            else
+            {
+                buttonPress = GUILayout.Button(text, instance._buildButtonStyle, GUILayout.ExpandWidth(true));
+            }
+
+            GUI.backgroundColor = instance._defaultBackgroundColor;
+
+            return buttonPress;
         }
 
         public static bool MoveUpButton()
